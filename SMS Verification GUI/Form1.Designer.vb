8e4996c379197ac0e39f7d6d83ff056f0675aff2
Exidous <exidous2008@gmail.com>
2016-05-21 14:18:02 -0500

    SMS Verification GUI

---
+++ SMS Verification GUI/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(111, 6)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(184, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Choose A Number"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(314, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Read Messages"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(5, 33)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(523, 221)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 261)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "SMS Verification GUI"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListView1 As ListView
+End Class
---
+++ SMS Verification GUI/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Dim LookupPage As String = "http://www.receive-sms-online.info/read-sms.php?phone="
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListView1.Columns.Add("From", 100, HorizontalAlignment.Center) 'Column 1
+        ListView1.Columns.Add("Body", 100, HorizontalAlignment.Center) 'Column 2
+        ListView1.Columns.Add("Date/Time", 100, HorizontalAlignment.Center) 'Column 3
+
+        Dim WC As New System.Net.WebClient
+        Dim Result As String = WC.DownloadString("http://www.receive-sms-online.info/")
+
+        Dim YourPage As String = ""
+        Dim X() As String = Split(Result, "read-sms.php?phone=")
+        Dim firstpass As Boolean = True
+        For Each y In X
+            If firstpass = True Then firstpass = False : Continue For
+            Dim XY() As String = Split(y, """")
+            XY = XY
+            ' Console.Title = "Your Phone # " & XY(0)
+            ComboBox1.Items.Add(XY(0))
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListView1.Items.Clear()
+        Dim WC As New System.Net.WebClient
+        Dim result As String = WC.DownloadString(LookupPage & ComboBox1.Text)
+        Dim X() As String = Split(result, "<tr>")
+        For i = 2 To X.Length - 1
+            Dim XY() As String = Split(X(i), "</td>")
+            If XY.Length = 1 Then Continue For
+            Dim From As String = Replace(XY(0), "<td>", "")
+            Dim Message As String = Replace(XY(1), "<td>", "")
+            Dim Whenz As String = Trim(Replace(XY(2), "<td>", ""))
+
+            Dim str(2) As String
+            Dim itm As ListViewItem
+            str(0) = From
+            str(1) = Message
+            str(2) = Whenz
+
+
+            itm = New ListViewItem(str)
+            ListView1.Items.Add(itm)
+            Application.DoEvents()
+        Next
+
+
+    End Sub
+End Class
